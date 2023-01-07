e25508d39d4f7250f2514417b3fe8e39ed543319
poseidon <61605433+poseidonKey@users.noreply.github.com>
2023-01-07 09:27:15 +0900

    complete

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,12 +1,53 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class HomeScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final PageController pageController = PageController();
+
+  @override
+  void initState() {
+    super.initState();
+    Timer.periodic(const Duration(seconds: 3), (timer) {
+      // print("excute!!");
+      int? nextPage = pageController.page?.toInt();
+      if (nextPage == null) {
+        return;
+      }
+      if (nextPage == 4) {
+        nextPage = 0;
+      } else {
+        nextPage++;
+      }
+      pageController.animateToPage(nextPage,
+          duration: const Duration(milliseconds: 500), curve: Curves.ease);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text("home Screen"),
+    // SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark);
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
+    return Scaffold(
+      body: PageView(
+        controller: pageController,
+        children: [1, 2, 3, 4, 5]
+            .map(
+              (e) => Image.asset(
+                "asset/img/image_$e.jpeg",
+                fit: BoxFit.cover,
+              ),
+            )
+            .toList(),
+      ),
     );
   }
 }
